53683bc7ecfda38c03000ba2a6cd4124eddca905
Jung <user@es302n33>
2018-10-02 10:44:00 +0800

    Varieble

--- my project/src/com/Jung/Hello.java
+++ my project/src/com/Jung/Hello.java
@@ -5,7 +5,14 @@ public class Hello {
 	public static void main (String[] args) {
 	
 		System.out.println("Hello world!");
-		
+		int age = 19;
+		int height = 178;
+		float weight = 54.1f;
+		String name = "Jung";
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
 	}
 		
 	}
